9950f11d2ed941ea61caef77323b77082f4fdd99
Michał Budzyński <michal@virtualdesign.pl>
2021-08-26 00:06:05 +0200

    add scene model

--- src/blueprints/blu_ground.ts
+++ src/blueprints/blu_ground.ts
@@ -1,4 +1,3 @@
-import {float} from "../../common/random.js";
 import {children} from "../components/com_children.js";
 import {collide} from "../components/com_collide.js";
 import {render_colored_shaded} from "../components/com_render.js";
@@ -11,7 +10,7 @@ export function blueprint_ground(game: Game) {
         collide(false, Layer.Terrain, Layer.None),
         rigid_body(RigidKind.Static),
         children([
-            transform([0, float(-0.2, 0.2), 0]),
+            transform(),
             render_colored_shaded(game.MaterialColoredShaded, game.MeshCube, [0, 1, 0.1, 1]),
         ]),
     ];
