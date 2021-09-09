aeaaf2597a2afbba14ef0a24796fb4186878caf0
Michał Budzyński <michal@virtualdesign.pl>
2021-09-09 02:02:54 +0200

    use blu_bush as tree crown

--- src/blueprints/blu_tree.ts
+++ src/blueprints/blu_tree.ts
@@ -1,11 +1,11 @@
-import {from_euler} from "../../common/quat.js";
-import {float, integer} from "../../common/random.js";
+import {float} from "../../common/random.js";
 import {children} from "../components/com_children.js";
 import {cull} from "../components/com_cull.js";
-import {render_colored_shadows, render_instanced} from "../components/com_render.js";
+import {render_colored_shadows} from "../components/com_render.js";
 import {transform} from "../components/com_transform.js";
 import {Game} from "../game.js";
 import {Has} from "../world.js";
+import {blueprint_bush} from "./blu_bush.js";
 
 // prettier-ignore
 export const leaft_colors = [
@@ -19,20 +19,7 @@ export const leaft_colors = [
 ];
 
 export function blueprint_tree(game: Game, min = 2, max = 4) {
-    let radius = float(0.5, 0.9);
-    let leaf_count = integer(400, 600);
     let height = float(min, max);
-    let offsets = [];
-    let rotations = [];
-    for (let i = 0; i < leaf_count; i++) {
-        offsets.push(
-            float(-radius, radius),
-            float(-radius, radius),
-            float(-radius, radius),
-            integer(0, 7)
-        );
-        rotations.push(...from_euler([0, 0, 0, 0], float(-90, 90), float(-90, 90), float(-90, 90)));
-    }
 
     return [
         children(
@@ -45,16 +32,7 @@ export function blueprint_tree(game: Game, min = 2, max = 4) {
                     [0.8, 0.2, 0.2, 1]
                 ),
             ],
-            [
-                transform([0, height, 0]),
-                cull(Has.Render),
-                render_instanced(
-                    game.MeshLeaf,
-                    Float32Array.from(offsets),
-                    Float32Array.from(rotations),
-                    leaft_colors
-                ),
-            ]
+            [transform([0, height, 0]), cull(Has.Render), ...blueprint_bush(game)]
         ),
     ];
 }
