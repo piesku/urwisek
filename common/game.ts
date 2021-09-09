2e14b9d69b5b64e69e5b67e70614ce031161480b
Staś Małolepszy <stas@piesku.com>
2021-09-10 01:59:16 +0200

    Remove framerate calculation

--- common/game.ts
+++ common/game.ts
@@ -163,17 +163,6 @@ export abstract class GameImpl {
 
     FrameReset(delta: number) {
         this.ViewportResized = false;
-
-        let update = performance.now() - this.Now;
-        if (update_span) {
-            update_span.textContent = update.toFixed(1);
-        }
-        if (delta_span) {
-            delta_span.textContent = (delta * 1000).toFixed(1);
-        }
-        if (fps_span) {
-            fps_span.textContent = (1 / delta).toFixed();
-        }
     }
 }
 
