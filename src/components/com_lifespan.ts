4f629c02d13dd4b2208229fd52e5081e45a4362c
Staś Małolepszy <stas@piesku.com>
2021-09-10 18:33:51 +0200

    Revert "Reimplement sys_lifespan as a Task"
    This reverts commit e6513025b570fe9fd21a6824bfa02e5d80490a2f.

--- src/components/com_lifespan.ts
+++ src/components/com_lifespan.ts
@@ -1,22 +1,23 @@
 /**
  * @module components/com_lifespan
- *
- * To save bytes, the Lifespan component is implemented as a Task.
  */
 
 import {Entity} from "../../common/world.js";
+import {Action} from "../actions.js";
 import {Game} from "../game.js";
 import {Has} from "../world.js";
-import {destroy_all} from "./com_children.js";
-import {TaskKind} from "./com_task.js";
 
-export function lifespan(duration: number) {
+export interface Lifespan {
+    Remaining: number;
+    Action?: Action;
+}
+
+export function lifespan(remaining: number, action?: Action) {
     return (game: Game, entity: Entity) => {
-        game.World.Signature[entity] |= Has.Task;
-        game.World.Task[entity] = {
-            Kind: TaskKind.Timeout,
-            Remaining: duration,
-            OnDone: (entity) => destroy_all(game.World, entity),
+        game.World.Signature[entity] |= Has.Lifespan;
+        game.World.Lifespan[entity] = {
+            Remaining: remaining,
+            Action: action,
         };
     };
 }
