b92f518de269ee1460ac0842503bad751dd6249a
Staś Małolepszy <stas@piesku.com>
2021-09-12 13:05:43 +0200

    mat_forward_particles_colored: Mangle names

--- materials/mat_forward_particles_colored.ts
+++ materials/mat_forward_particles_colored.ts
@@ -3,39 +3,39 @@ import {GL_POINTS} from "../common/webgl.js";
 import {ParticlesColoredLayout} from "./layout.js";
 
 let vertex = `#version 300 es\n
-    uniform mat4 pv;
-    uniform vec4 color_start;
-    uniform vec4 color_end;
+    uniform mat4 A;
+    uniform vec4 B;
+    uniform vec4 C;
     // [x: lifespan, y: speed, z: size_start, w: size_end];
-    uniform vec4 details;
+    uniform vec4 D;
 
     // [x, y, z, w: age]
-    in vec4 attr_origin_age;
-    in vec3 attr_direction;
+    in vec4 a;
+    in vec3 b;
 
-    out vec4 vert_color;
+    out vec4 j;
 
     void main() {
         // Move the particle along the direction axis.
-        vec3 velocity = attr_direction * details.y;
-        gl_Position = pv * vec4(attr_origin_age.xyz + velocity * attr_origin_age.w, 1.0);
+        vec3 o = b * D.y;
+        gl_Position = A * vec4(a.xyz + o * a.w, 1.0);
 
         // Interpolate color and size.
-        float t = attr_origin_age.w / details.x;
-        gl_PointSize = mix(details.z, details.w, t);
-        vert_color = mix(color_start, color_end, t);
+        float t = a.w / D.x;
+        gl_PointSize = mix(D.z, D.w, t);
+        j = mix(B, C, t);
     }
 `;
 
 let fragment = `#version 300 es\n
     precision mediump float;
 
-    in vec4 vert_color;
+    in vec4 j;
 
-    out vec4 frag_color;
+    out vec4 f;
 
     void main() {
-        frag_color = vert_color;
+        f = j;
     }
 `;
 
@@ -47,14 +47,14 @@ export function mat_forward_particles_colored(
         Mode: GL_POINTS,
         Program: program,
         Locations: {
-            Pv: gl.getUniformLocation(program, "pv")!,
+            Pv: gl.getUniformLocation(program, "A")!,
 
-            ColorStart: gl.getUniformLocation(program, "color_start")!,
-            ColorEnd: gl.getUniformLocation(program, "color_end")!,
-            Details: gl.getUniformLocation(program, "details")!,
+            ColorStart: gl.getUniformLocation(program, "B")!,
+            ColorEnd: gl.getUniformLocation(program, "C")!,
+            Details: gl.getUniformLocation(program, "D")!,
 
-            OriginAge: gl.getAttribLocation(program, "attr_origin_age")!,
-            Direction: gl.getAttribLocation(program, "attr_direction")!,
+            OriginAge: gl.getAttribLocation(program, "a")!,
+            Direction: gl.getAttribLocation(program, "b")!,
         },
     };
 }
