af2f5c8a9c10c54dc836d5059a215cff3380839b
Staś Małolepszy <stas@piesku.com>
2021-09-12 13:08:14 +0200

    mat_forward_particles_colored: Remove whitespace

--- materials/mat_forward_particles_colored.ts
+++ materials/mat_forward_particles_colored.ts
@@ -2,42 +2,10 @@ import {link, Material} from "../common/material.js";
 import {GL_POINTS} from "../common/webgl.js";
 import {ParticlesColoredLayout} from "./layout.js";
 
-let vertex = `#version 300 es\n
-    uniform mat4 A;
-    uniform vec4 B;
-    uniform vec4 C;
-    // [x: lifespan, y: speed, z: size_start, w: size_end];
-    uniform vec4 D;
+let vertex =
+    "#version 300 es\nuniform mat4 A;uniform vec4 B,C,D;in vec4 a;in vec3 b;out vec4 j;void main(){vec3 o=b*D.y;gl_Position=A*vec4(a.xyz+o*a.w,1.);float t=a.w/D.x;gl_PointSize=mix(D.z,D.w,t);j=mix(B,C,t);}";
 
-    // [x, y, z, w: age]
-    in vec4 a;
-    in vec3 b;
-
-    out vec4 j;
-
-    void main() {
-        // Move the particle along the direction axis.
-        vec3 o = b * D.y;
-        gl_Position = A * vec4(a.xyz + o * a.w, 1.0);
-
-        // Interpolate color and size.
-        float t = a.w / D.x;
-        gl_PointSize = mix(D.z, D.w, t);
-        j = mix(B, C, t);
-    }
-`;
-
-let fragment = `#version 300 es\n
-    precision mediump float;
-
-    in vec4 j;
-
-    out vec4 f;
-
-    void main() {
-        f = j;
-    }
-`;
+let fragment = "#version 300 es\nprecision mediump float;in vec4 j;out vec4 f;void main(){f=j;}";
 
 export function mat_forward_particles_colored(
     gl: WebGL2RenderingContext
