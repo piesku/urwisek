a003bc41175cdb343951564c76b758fc88bdd998
Staś Małolepszy <stas@piesku.com>
2021-09-12 20:42:36 +0200

    Minify a bit more

--- materials/mat_forward_instanced.ts
+++ materials/mat_forward_instanced.ts
@@ -3,7 +3,7 @@ import {GL_TRIANGLES} from "../common/webgl.js";
 import {FogLayout, InstancedLayout, SingleColorLayout} from "./layout.js";
 
 let vertex =
-    "#version 300 es\nuniform mat4 A,B;uniform vec3 C;uniform vec4 D;in vec3 a,b,c,d,e;out vec4 j;void main(){vec4 o=B*mat4(mat3(b,c,d))*vec4(a+e,1.);gl_Position=A*o;j=vec4(.02,.06,.04,1.);j=mix(j,D,smoothstep(0.,1.,clamp(0.,1.,length(C-o.xyz)/15.)));}";
+    "#version 300 es\nuniform mat4 A,B;uniform vec3 C;uniform vec4 D;in vec3 a,b,c,d,e;out vec4 j;void main(){vec4 o=B*mat4(mat3(b,c,d))*vec4(a+e,1.);gl_Position=A*o;j=mix(vec4(.02,.06,.04,1.),D,smoothstep(0.,1.,clamp(0.,1.,length(C-o.xyz)/15.)));}";
 
 let fragment = "#version 300 es\nprecision mediump float;in vec4 j;out vec4 f;void main(){f=j;}";
 
--- materials/mat_forward_particles_colored.ts
+++ materials/mat_forward_particles_colored.ts
@@ -3,7 +3,7 @@ import {GL_POINTS} from "../common/webgl.js";
 import {FogLayout, ParticlesColoredLayout} from "./layout.js";
 
 let vertex =
-    "#version 300 es\nuniform mat4 A;uniform vec4 D,E,F,G;uniform vec3 C;in vec4 a;in vec3 b;out vec4 j;void main(){vec3 p=b*G.y;vec4 o=vec4(a.xyz+p*a.w,1.);gl_Position=A*o;float t=a.w/G.x;gl_PointSize=mix(G.z,G.w,t);j=mix(E,F,t);j=mix(j,D,smoothstep(0.,1.,clamp(0.,1.,length(C-o.xyz)/15.)));}";
+    "#version 300 es\nuniform mat4 A;uniform vec4 D,E,F,G;uniform vec3 C;in vec4 a;in vec3 b;out vec4 j;void main(){vec3 p=b*G.y;vec4 o=vec4(a.xyz+p*a.w,1.);gl_Position=A*o;float t=a.w/G.x;gl_PointSize=mix(G.z,G.w,t);j=mix(mix(E,F,t),D,smoothstep(0.,1.,clamp(0.,1.,length(C-o.xyz)/15.)));}";
 
 let fragment = "#version 300 es\nprecision mediump float;in vec4 j;out vec4 f;void main(){f=j;}";
 
