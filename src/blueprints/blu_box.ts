0071680db50044127741404bae9899e5f28ed22e
Staś Małolepszy <stas@piesku.com>
2021-09-12 23:31:35 +0200

    Revert "Push boxes at the same speed as you pull them"
    This reverts commit 1448c76b8def18c436f2006601f84b64a7529a97.

--- src/blueprints/blu_box.ts
+++ src/blueprints/blu_box.ts
@@ -8,11 +8,7 @@ import {Has} from "../world.js";
 
 export function blueprint_box(game: Game) {
     return [
-        collide(
-            true,
-            Layer.Movable | Layer.SurfaceWood,
-            Layer.Terrain | Layer.Movable | Layer.Player
-        ),
+        collide(true, Layer.Movable | Layer.SurfaceWood, Layer.Terrain | Layer.Movable),
         rigid_body(RigidKind.Dynamic),
         mimic(0),
         disable(Has.Mimic),
