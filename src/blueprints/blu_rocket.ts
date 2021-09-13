a241cca04a22fb66520497f4cc8bac76b1d21b80
Staś Małolepszy <stas@piesku.com>
2021-09-13 02:10:35 +0200

    Remove AudioSource from intro rocket

--- src/blueprints/blu_rocket.ts
+++ src/blueprints/blu_rocket.ts
@@ -1,9 +1,7 @@
 import {from_euler} from "../../common/quat.js";
 import {float} from "../../common/random.js";
-import {audio_source} from "../components/com_audio_source.js";
 import {children} from "../components/com_children.js";
 import {control_always} from "../components/com_control_always.js";
-import {disable} from "../components/com_disable.js";
 import {emit_particles} from "../components/com_emit_particles.js";
 import {lifespan} from "../components/com_lifespan.js";
 import {move} from "../components/com_move.js";
@@ -12,16 +10,12 @@ import {shake} from "../components/com_shake.js";
 import {transform} from "../components/com_transform.js";
 import {Game} from "../game.js";
 import {prop_rocket} from "../props/prop_rocket.js";
-import {snd_rocket} from "../sounds/snd_rocket.js";
-import {Has} from "../world.js";
 
 export function blueprint_rocket(game: Game) {
     return [
         control_always([0, 0, 1], null),
         move(float(1, 3), 0),
         lifespan(25),
-        audio_source(snd_rocket),
-        disable(Has.AudioSource),
         children(
             // Body 1.
             [
