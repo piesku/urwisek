22128e9c673718865e8a69e655e6d223d52c3081
Staś Małolepszy <stas@piesku.com>
2021-09-13 11:52:40 +0200

    snd_ping when a cub is found

---
+++ src/sounds/snd_ping.ts
@@ -0,0 +1,7 @@
+import {AudioSynthClip} from "../../common/audio.js";
+
+export let snd_ping: AudioSynthClip = {
+    Instrument: [5, "lowpass", 8, 8, false, false, 8, 8, [["sine", 8, 2, 0, 8, 8, false]]],
+    Notes: [72, 74, 76],
+    Exit: 1,
+};
