7890079c17e5576ecc01acedf549ed8db92c923e
Staś Małolepszy <stas@piesku.com>
2021-09-18 11:28:27 +0200

    Restore snd_ping

--- src/sounds/snd_ping.ts
+++ src/sounds/snd_ping.ts
@@ -1,7 +1,20 @@
 import {AudioSynthClip} from "../../common/audio.js";
 
 export let snd_ping: AudioSynthClip = {
-    Instrument: [5, "lowpass", 8, 8, false, false, 8, 8, [["sine", 8, 2, 0, 8, 8, false]]],
-    Notes: [72, 74, 76],
-    Exit: 1,
+    Instrument: [
+        5,
+        "lowpass",
+        8,
+        0,
+        false,
+        false,
+        8,
+        8,
+        [
+            ["sine", 7, 3, 4, 10, 8, false],
+            ["triangle", 7, 3, 4, 11, 2, false],
+        ],
+    ],
+    Notes: [66, 68, 70],
+    Exit: 5,
 };
